6eae783dc11baba009f6a4ba2fe5018afce7c7a3
xoftware <james.ryan.stortz@gmail.com>
2016-11-13 23:49:43 +0000

    GG

---
+++ GG.PLAY.LINUX.sh
@@ -0,0 +1,5 @@
+./bin/ggx3-client.x86_64 	\
+ +set fs_basepath "mod/"	\
+ +set com_basegame baseGG 	\
+ +set fs_game InstaUnlagged 	\
+ +exec demo
---
+++ GG.PLAY.WINDOWS.bat
@@ -0,0 +1,7 @@
+start bin\ggx3-client.exe ^
++set fs_basepath "mod\" ^
++set com_gamename GoodGame ^
++set com_basegame baseGG ^
++set fs_game InstaUnlagged ^
++exec demo ^
++set sv_pure 0
